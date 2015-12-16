7d640163e102240748d93d286d80fbcfa2d34391
Oisin100 <minedudes2012@gmail.com>
2015-12-16 21:52:05 +0100

    Revert "Revert "Feature: Save system(Untested)""
    This reverts commit 41870c05e6943750f9994d13c9e4d27c40e2601b.

--- src/net/minemix/kitpvp/onevsone/OneVsOneManager.java
+++ src/net/minemix/kitpvp/onevsone/OneVsOneManager.java
@@ -1,11 +1,16 @@
 package net.minemix.kitpvp.onevsone;
 
 import net.minemix.kitpvp.KitPvp;
+import net.minemix.kitpvp.onevsone.arena.ArenaManager;
 
 public class OneVsOneManager {
+	
+	public ArenaManager manager;
 
 	public OneVsOneManager() {
-		KitPvp.instance.getServer().getPluginManager().registerEvents(new OneVsOneListeners(), KitPvp.instance);
+		KitPvp.instance.getServer().getPluginManager().registerEvents(new OneVsOneListeners(this), KitPvp.instance);
+		
+		manager = new ArenaManager();
 	}
 	
 }
